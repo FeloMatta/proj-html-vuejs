3fdfc26a85c2b461a1083e9d53898bbcbcda24bf
FeloMatta <mattafelo@gmail.com>
2023-02-15 17:08:25 +0100

    All the latest news completato

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -47,46 +47,168 @@ export default{
           All the <span class="my-underline">latest</span> news
         </h1>
       </div>
-      <div class="container text-center py-5">
+      <div class="container py-5">
         <div class="row">
-        <div class="col">
-          <img src="../assets/img/post_feat_img_25-320x202.jpg" alt="">
-          <h4>
-            Morbi vitae dui euismod vulputate sollicitudin
-          </h4>
-          <p>
-            October
-          </p>
-          <p class="py-2">
-            Donec
-          </p>
-        </div>
-        <div class="col">
-          <img src="../assets/img/post_feat_img_24-320x202.jpg" alt="">
-          <h4>
-            Morbi vitae
-          </h4>
-          <p>
-            October
-          </p>
-          <p class="py-2">
-            Donec
-          </p>
-        </div>
-        <div class="col">
-          <img src="../assets/img/post_feat_img_23-320x202.jpg" alt="">
-          <h4>
-            Morbi vitae
-          </h4>
-          <p>
-            October
-          </p>
-          <p class="py-2">
-            Donec
-          </p>
+          <div class="col-4 d-flex flex-column  text-start">
+            <img src="../assets/img/post_feat_img_25-320x202.jpg" alt="">
+            <h2 class="fw-bolder fs-6 text-start py-2">
+              Morbi vitae dui euismod vulputate sollicitudin
+            </h2>
+            <p class="text-start my-p">
+              October 11th, 2015 | <span>2 comments</span>
+            </p>
+            <p class="py-2 text-start">
+              Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl
+            </p>
+          </div>
+          <div class="col-4 d-flex flex-column  text-start">
+            <img src="../assets/img/post_feat_img_24-320x202.jpg" alt="">
+            <h2 class="fw-bold fs-6 py-2">
+              Vivamus pellenteque, felis quis varius
+            </h2>
+            <p class="text-start my-p">
+              October 11th, 2015 | Comments Off
+            </p>
+            <p class="py-2 text-start">
+              Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl
+            </p>
+          </div>
+          <div class="col-4 d-flex flex-column  text-start">
+            <img src="../assets/img/post_feat_img_23-320x202.jpg" alt="">
+            <h2 class="fw-bold fs-6 py-2">
+              Donec ornare pretium eget scelisque justo
+            </h2>
+            <p class="text-start my-p">
+              October 11th, 2015 | Comments Off
+            </p>
+            <p class="py-2 text-start">
+              Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl
+            </p>
+          </div>
         </div>
       </div>
-</div>
+      <!-- FEATURED ARTICLE -->
+      <div id="fetured-article" class="container d-flex flex-column justify-content-end align-items-start">
+        
+        <p class=" text-start py-3 fs-4">
+          FEATURED ARTICLE
+        </p>
+        <h1 class= "text-start">
+          Cras malesuada et orci eget pharetra
+        </h1>
+        <p class="text-start py-4">
+          in quis lectus sed leo elementum faucibus in dapibus dictum. Nullamolestie tortor nec lectus venenatis, sed blandit dui, dolor at bibendum sadips ets ipsums dolores ficilis uns leo lectus.
+        </p>
+        <button class="btn py-3 px-5 rounded-1" type="button">
+          READ MORE >
+        </button>
+      </div>
+      <!-- /FEATURED ARTICLE -->
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-4 d-flex flex-column  text-start">
+            <img src="../assets/img/post_feat_img_22-320x202.jpg" alt="">
+            <h2 class="fw-bolder fs-6 text-start py-2">
+              Fusce sollicitudin nunc sed placerat varius
+            </h2>
+            <p class="text-start my-p">
+              October 11th, 2015 | Comments Off
+            </p>
+            <p class="py-2 text-start">
+              Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl
+            </p>
+          </div>
+          <div class="col-4 d-flex flex-column  text-start">
+            <img src="../assets/img/post_feat_img_21-320x202.jpg" alt="">
+            <h2 class="fw-bold fs-6 py-2">
+              Donec facilis sodales leo sit amet laoreet
+            </h2>
+            <p class="text-start my-p">
+              October 11th, 2015 | Comments Off
+            </p>
+            <p class="py-2 text-start">
+              Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl
+            </p>
+          </div>
+          <div class="col-4 d-flex flex-column  text-start">
+            <img src="../assets/img/post_feat_img_20-320x202.jpg" alt="">
+            <h2 class="fw-bold fs-6 py-2">
+              Proin eu purus sed aru aliquet curabir vens
+            </h2>
+            <p class="text-start my-p">
+              October 11th, 2015 | Comments Off
+            </p>
+            <p class="py-2 text-start">
+              Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- REVIEWED PRODUCT // TUTORIALS & GUIDES -->
+      <div class="container py-5">
+        <div class="row">
+          <div id="reviewed-product" class="col-8">
+            <p class=" text-start py-3 fs-4">
+              REVIEWED PRODUCT
+            </p>
+            <h1 class= "text-start">
+              Mauris viverra atisan ipsum eget felis prims efficitur varius
+            </h1>
+            <p class="text-start py-4">
+              in quis lectus sed leo elementum faucibus in dapibus dictum. Nullamolestie tortor nec lectus venenatis, sed blandit dui, dolor at bibendum.
+            </p>
+            <button class="btn py-3 px-5 rounded-1" type="button">
+              READ MORE >
+            </button>
+          </div>
+          <!-- TUTORIALS & GUIDES -->
+          <div id="tutorials" class="col-4 d-flex flex-column justify-content-around px-4">
+            <h2 class="fw-bold">
+              <span class="my-underline">Tutorials</span> & Guides
+            </h2>
+            <div class="d-flex">
+              <img src="../assets/img/post_feat_img_23-147x118.jpg" alt="">
+              <div class="d-flex flex-column px-3">
+                <h2 class="fw-bold fs-6 py-2">
+                  Donec ornare pretium eget scelisque justo
+                </h2>
+                <p class="text-start mini-p">
+                  October 11th, 2015 | Comments Off
+                </p>
+              </div>
+            </div>
+            <div class="d-flex">
+              <img src="../assets/img/post_feat_img_22-147x118.jpg" alt="">
+              <div class="d-flex flex-column px-3">
+                <h2 class="fw-bolder fs-6 text-start py-2">
+                  Fusce sollicitudin nunc sed placerat varius
+                </h2>
+                <p class="text-start mini-p">
+                  October 11th, 2015 | Comments Off
+                </p>
+              </div>
+            </div>
+            <div class="d-flex">
+              <img src="../assets/img/post_feat_img_20-147x118.jpg" alt="">
+              <div class="d-flex flex-column px-3">
+                <h2 class="fw-bold fs-6 py-2">
+                  Proin eu purus sed aru aliquet curabir vens
+                </h2>
+                <p class="text-start mini-p">
+                  October 11th, 2015 | Comments Off
+                </p>
+              </div>
+            </div>
+          </div>
+          <!-- /TUTORIALS & GUIDES -->
+        </div>
+      </div>
+      <!-- /REVIEWED PRODUCT // TUTORIALS & GUIDES -->
+      <!-- READ OUR BLOG -->
+      <div>
+        
+      </div>
+      <!-- /READ OUR BLOG -->
     </div>
     <!-- / ALL THE LATEST NEWS -->
   </main>
@@ -98,6 +220,7 @@ main{
 
   #first-section{
     background-image: url(../assets/img/home_slider_bg.jpg);
+    background-size: cover;
     object-fit:cover;
     height: 880px;
     
@@ -153,6 +276,67 @@ main{
       padding-bottom: 20px;
       border-bottom: 2px solid #ff8d61;
       line-height: 48px;
+    }
+    .row{
+      .my-p{
+          font-size: 0.9rem;
+        }
+      p{
+        color: #707070;
+        font-weight: normal;
+        
+        span{
+          color: black;
+          font-weight: bold;
+        }
+      }
+      .col{
+        
+
+        img{
+          width: 100%;
+        }
+        
+      }
+    }
+
+    #fetured-article{
+      background-image: url(../assets/img/featured_article_1_bg.jpg);
+      background-size: cover;
+      object-fit: cover;
+      height: 650px;
+      color: white;
+      padding: 90px;
+
+      button{
+        background-color: black;
+        color: white;
+        font-weight: bold;
+      }
+    }
+
+    #reviewed-product{
+      background-image: url(../assets/img/featured_article_2_bg.jpg);
+      background-size: cover;
+      height: 550px;
+      color: white;
+      padding: 125px;
+
+      p{
+        color: white;
+      }
+
+      button{
+        background-color: black;
+        color: white;
+        font-weight: bold;
+      }
+    }
+
+    #tutorials{
+      .mini-p{
+        font-size: 0.8rem;
+      }
     }
   }
 }
